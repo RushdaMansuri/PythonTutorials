e9440a0233ff44ded1c2a1f52fc9f27624c3cefd
rmansuri <mansurirushda7@gmail.com>
2024-02-12 21:45:51 -0500

    printing variables

---
+++ 1. Basics/printing_variables.py
@@ -0,0 +1,18 @@
+name = "Rushda"
+age = 20
+gender = "female"
+
+# Method 1
+print(name, age, gender)
+
+# Method 2 with commas
+print("My name is ", name)
+print("My age is ", age)
+print("My gender is ", gender)
+
+# Method 3 (F-strings)
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My gender is {gender}")
+
+print(f"{name = }")  # sued for debugging

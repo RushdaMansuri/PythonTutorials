a01dbf427b3ebfc96ed9106d40e2ff23712b53c7
rmansuri <mansurirushda7@gmail.com>
2024-02-25 14:02:07 -0500

    Added Question 8,9

---
+++ Assignment 1/q9.py
@@ -0,0 +1,9 @@
+# Take values of length and breadth of a rectangle from user and check if it is square or not
+
+length = int(input("Enter the length of the rectangle"))
+breadth = int(input("Enter the breadth of the rectangle"))
+
+if length == breadth:
+    print("It's a square")
+else:
+    print("It's not a square")

a26ce346350f473638c7f83f0acf0a39a0041937
rmansuri <mansurirushda7@gmail.com>
2024-02-25 13:22:06 -0500

    Assignments Q4, Q5

---
+++ Assignment 1/q5.py
@@ -0,0 +1,5 @@
+# Calculate Area of Square by taking side from User.
+
+length = int(input("Enter Length of Square:"))
+area = length * length
+print("Area of Square: ", area)
---
+++ Assignment 1/q6.py
@@ -0,0 +1,3 @@
+# Ask number of games played in a tournament. Ask the user number of
+# games won and number of games loss. Calculate number of tie and total
+# Points. (1 win= 4 points, 1 tie =2 points)
